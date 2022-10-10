bb9359a713ade006e66293a9394b8d5dd06b334c
evolF35 <maccyair@gmail.com>
2022-10-09 18:28:28 -0700

    added giphy photos

--- myscripts.js
+++ myscripts.js
@@ -1,12 +1,23 @@
 
-
 let wheather_data = document.querySelector(".wheatherinfo");
 
+
+let place = document.querySelector(".name");
+let image = document.querySelector("img");
+let cWeather = document.querySelector(".weather");
+let cTemp = document.querySelector(".currentTemp");
+let humidity = document.querySelector(".humidity");
+let wind = document.querySelector(".wind");
+
+
 async function getWheather(input) {
+
     let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input}&APPID=bb1970edca621eaec570c9e3e45940c0`);
     let data = await response.json();
 
     console.log(data);
+
+    renderWheather(data);
 }
 
 
@@ -19,5 +30,33 @@ enterButton.addEventListener('click', () =>{
     console.log(inputText);
     
     getWheather(inputText);
-
 })
+
+
+
+function renderWheather(data) {
+
+    place.innerText = data.name;
+    
+    getPic(data);
+
+    cWeather.innerText = data.weather[0].description;
+    cTemp.innerText = "Temperature: " + Math.floor(+data.main.temp - 273.15) + " \u00B0C";
+    humidity.innerText = "Humidity: " + data.main.humidity + "%";
+    wind.innerText = "Wind Speed: " + data.wind.speed + " m/s";
+
+}
+
+
+async function getPic(info){
+
+    let imgDescription = info.weather[0].description + info.name;
+
+    let response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=MBnLIaKTWifuoAMNwIY3dvrjz0CjHuPJ&s=${imgDescription}`,{mode:"cors"});
+
+    let pic = await response.json();
+
+    console.log(pic);
+
+    image.src = pic.data.images.original.url;
+}
